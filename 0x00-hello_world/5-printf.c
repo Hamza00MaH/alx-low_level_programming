39ffa6628cee9c26425617f675ff1821b4ae329b
Hamza <Hamza@email.com>
2023-03-16 16:46:13 -0700

    lawr

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - our fucntion/entry point
+ * Description: printf a sidi
+ *
+ * Return: Always 0 integer
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
+}
