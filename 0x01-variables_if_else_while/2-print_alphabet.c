305b3c973d41408462ce2b93c3ca6f160b55b4d0
Hamza <Hamza@email.com>
2023-03-17 16:31:08 -0700

    2sec

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+	 * main -our entrypoint
+	 * Description:  lowercases alphabet on ASCll,
+	 * Return: 0.
+*/
+int main(void)
+{
+int lwa;
+for (lwa = 97; lwa <= 122; lwa++)
+{
+putchar(lwa);
+}
+putchar('\n');
+return (0);
+}
