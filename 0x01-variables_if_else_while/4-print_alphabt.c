be576e71914b701a0f19fd67313e385e5ca2c433
Hamza <Hamza@email.com>
2023-03-17 17:22:57 -0700

    2sec

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+	 * main -our entrypoint
+	 * Description:  lowercases alphabet on ASCll,
+	 * Return: 0.
+*/
+int main(void)
+{
+int lwa;
+for (lwa = 97; lwa <= 122; lwa++)
+char ltr = 'a' + lwa;
+if(ltr == 'q' || ltr == 'e')
+{}
+else{
+putchar(lwa);
+}
+putchar('\n');
+return (0);
+}
