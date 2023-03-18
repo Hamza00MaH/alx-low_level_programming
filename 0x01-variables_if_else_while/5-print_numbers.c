1be3d466cff2ebda83452219c20d924dce9bb8fd
Hamza <Hamza@email.com>
2023-03-17 17:52:48 -0700

    lol

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+	 * main -our entrypoint
+	 * Description:  all single digital numbers,
+	 * Return: 0.
+*/
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
