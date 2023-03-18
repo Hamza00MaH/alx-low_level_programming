a141fc82bb57ce9d63732a5f80f58e8fe3f378fc
Hamza <Hamza@email.com>
2023-03-17 18:05:14 -0700

    lol

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+	 * main -our entrypoint
+	 * Description:  lowercases alphabet on ASCll revers.sd,
+	 * Return: 0.
+*/
+int main(void)
+{
+int lwa;
+for (lwa = 'z'; lwa >= 'a'; lwa--)
+{
+putchar(lwa);
+}
+putchar('\n');
+return (0);
+}
