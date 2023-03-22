242bbda7f45abe1db4f913f9de7a600b1f5e823f
Hamza <Hamza@email.com>
2023-03-21 17:30:10 -0700

    oki

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+	 * main -our entrypoint
+	 * Description:  print _putchar ,
+	 * Return: 0.
+*/
+int main(void)
+{
+_putchart('_');
+_putchart('p');
+_putchart('u');
+_putchart('t');
+_putchart('c');
+_putchart('h');
+_putchart('a');
+_putchart('r');
+_putchart('t');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/**
+	 * main -our entrypoint
+	 * Description:  prints the alphabet, in lowercase, followed by a new line.,
+	 * Return: 0.
+*/
