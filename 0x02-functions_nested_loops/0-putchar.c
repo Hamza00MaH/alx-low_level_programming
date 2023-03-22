809920a76f235e2e726f91afcd5c14ab1ab0fd01
Hamza <Hamza@email.com>
2023-03-21 17:33:35 -0700

    oki

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-_putchart('_');
-_putchart('p');
-_putchart('u');
-_putchart('t');
-_putchart('c');
-_putchart('h');
-_putchart('a');
-_putchart('r');
-_putchart('t');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('t');
 return (0);
 }
