4a5a554c65ea2720eb3dc9ac5ff9289e7beae886
Hamza <Hamza@email.com>
2023-03-23 19:25:37 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main -our entry Point
+* Description: FizzBuzz game
+* Return: 0 Success
+*/
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz");
+}
+if (i % 5 == 0)
+{
+printf("Buzz");
+}
+if ((i % 3 != 0) && (i % 5 != 0))
+{
+printf("%d", i);
+}
+if (i != 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
