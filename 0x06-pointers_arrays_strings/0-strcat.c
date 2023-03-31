bfc69d904c3cf10558f19301930462d68ed0088a
Hamza <Hamza@email.com>
2023-03-30 18:11:28 -0700

    first commit: task 0 and all file needed

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates-two-strings
+ * @dest: Destination string
+ * @src: Source string
+ * Return: dest
+ */
+
+char _strcat(char *dest, char *src)
+{
+int dest_length = 0;
+int tog = 0;
+while (dest[dest_length] != '\0')
+{
+dest_length++;
+}
+while (src[tog] != '\0')
+{
+dest[dest_length] = src[tog];
+tog++;
+dest_length++;
+}
+dest[dest_length] = '\0';
+return (dest);
+}
