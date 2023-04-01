cdbdcdf2988c06f01be12dddc8ac767732295edd
Hamza <Hamza@email.com>
2023-03-31 17:30:13 -0700

    6 task cap words of string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - cap-words-in-array
+ * @cap: to cap
+ * Return: cap
+ */
+
+char *cap_string(char *cap)
+{
+int i = 0;
+while (cap[i] != '\0')
+{
+for (cap[i] > 96 && cap[i] < 123)
+if (i == 0)
+{
+cap[i] -= 32;
+}
+if (strchr(" \t\n,;.!?\"(){}|", cap[i - 1]) != NULL)
+{
+cap[i] -= 32;
+}
+i++;
+}
+return (cap);
+}
