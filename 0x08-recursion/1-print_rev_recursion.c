fa996ceb81dd95ed1c1ab28f052169a480f02c45
Hamza <Hamza@email.com>
2023-04-21 22:33:49 -0700

    1 task recur

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - our function that print string revrersed
+* @s:string to print
+* Description: print the name
+* Return: void
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (*s != '0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+else
+{
+_putchar('\n');
+}
+}
