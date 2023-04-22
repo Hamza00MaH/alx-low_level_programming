02d2e8fd7fe9f623a817b0c4420947539af3016e
Hamza <Hamza@email.com>
2023-04-21 22:12:03 -0700

    0 task recur

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _puts_recursion - our function that print string
+* @s:string to print
+* Description: print the name
+* Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_putchar(*s);
+_puts(s + 1);
+}
+}
