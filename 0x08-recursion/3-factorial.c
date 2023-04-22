59ccb6ee4157e4445d6c87e8dfe7c6b0d0e45ae7
Hamza <Hamza@email.com>
2023-04-21 23:11:49 -0700

    3 task recur

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* factorial - our function that returns factorial
+* @n:var
+* Description: factorial res
+* Return: factorial
+*/
+
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else
+{
+return (factorial(n - 1) * n);
+}
+}
