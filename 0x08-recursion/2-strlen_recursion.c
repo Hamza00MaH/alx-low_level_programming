0ad5eb1776720b33ad136256f9054bd832e2906a
Hamza <Hamza@email.com>
2023-04-21 22:54:51 -0700

    2 task recur

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,14 +4,14 @@
 * _strlen_recursion - our function that returns length of str
 * @s:string to measure
 * Description: print the length
-* Return: length
+* Return: lemgth
 */
 
 
 int _strlen_recursion(char *s)
 {
 if (*s != '\0')
-return _strlen_recursion(s + 1);
+_strlen_recursion(s + 1);
 else
 return (0);
 }
