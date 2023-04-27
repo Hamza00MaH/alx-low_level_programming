fc576a3b422eabdf434bd56cd517edc29bebedc1
Hamza <Hamza@email.com>
2023-04-27 13:32:12 -0700

    0 task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,15 @@
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
+while (h)
+{
 if (!h->str)
 {
 printf("[0] (nil)\n");
 }
-while (h != NULL)
-{
+else
 printf("[%ld] %s\n", h->len, h->str);
-h = h -> next;
+h = h->next;
 count++;
 }
 return (count);
