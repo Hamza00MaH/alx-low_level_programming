0545f2d47cd816302e0b80f15bf74c2a132b1ba8
Hamza <Hamza@email.com>
2023-04-27 18:34:10 -0700

    4 task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+* free_list - our function that frees node
+* @head: pointer to the head of the Linkded-List
+* Return: void
+*/
+
+void free_list(list_t *head)
+{
+if (head == NULL)
+return;
+free_list(head->next);
+free (head);
+}
