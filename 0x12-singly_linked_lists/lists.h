332c03b47f1fd65778c0940b2ee66a12b16115d2
Hamza <Hamza@email.com>
2023-04-27 17:38:43 -0700

    description add

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,13 +14,13 @@
  * Description: singly linked list node structure
  */
 
-struct list
+struct list_s
 {
 char *str;
 unsigned int len;
-struct list *next;
+struct list_s *next;
 };
-typedef struct list list_t;
+typedef struct list_s list_t;
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
