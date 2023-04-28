53b944de0f8233e79fb443bcc08736537121ff15
Hamza <Hamza@email.com>
2023-04-27 19:06:02 -0700

    before the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,9 @@
 * Return: void
 */
 
-void before_main() __attribute__ ((constructor));
-void before_main()
+void before_main(void) __attribute__ ((constructor));
+void before_main(void)
 {
-  printf("You're beat! and yet, you must allow,\n");
-  printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
