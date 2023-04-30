50393404648835b79537fd6e1723cc4beeb05794
Hamza <Hamza@email.com>
2023-04-30 09:50:54 -0700

    add init_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - our struct for dogs
  * @name: name of dog
@@ -17,5 +20,7 @@ float age;
 char *owner;
 };
 
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /*DOG_H*/
