dfa384b5069000146e7eed6ee164df39413bc421
Hamza <Hamza@email.com>
2023-04-30 08:55:00 -0700

    struct first comm

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - our struct for dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: simple struct regrp some info about dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+
+#endif /*DOG_H*/
