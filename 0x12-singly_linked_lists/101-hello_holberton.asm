d12f4f270cf3b4fbd6e8faedf3c94b44f7d9d37b
Hamza <Hamza@email.com>
2023-04-30 07:38:59 -0700

    assembled

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,44 @@
+	.file	"assembly.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello, Holberton"
+	.text
+	.globl	print_assembly
+	.type	print_assembly, @function
+print_assembly:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	print_assembly, .-print_assembly
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
