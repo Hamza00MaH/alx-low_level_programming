9bf850b0d9d7d23569bc4e044fc582018ca6e4bf
Hamza <Hamza@email.com>
2023-05-01 16:46:16 -0700

    finito

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - function freedom for dog data
+ * @d: poiter to typedef struct
+ * Description: free memory that contain data of dog
+ */
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+free((*d).name);
+free((*d).owner);
+free(d);
+}
