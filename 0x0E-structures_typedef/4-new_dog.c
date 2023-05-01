80dc1a6980ba8ce487f915da6dcca8af4084c37a
Hamza <Hamza@email.com>
2023-05-01 16:15:50 -0700

    new dogs add

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,37 @@
+#include "dog.h"
+
+/**
+ * new_dog - function that create new dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ * Description: create new dog struct
+ * Return:  new dog info
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *new;
+if (name == NULL || age < '\0' || owner == NULL)
+return (NULL);
+new = malloc(sizeof(dog_t));
+if (new == NULL)
+{
+return (NULL);
+}
+(*new).name = strdup(name);
+if ((*new).name == NULL)
+{
+free(new);
+return (NULL);
+}
+(*new).age = age;
+(*new).owner = strdup(owner);
+if ((*new).owner == NULL)
+{
+free((*new).name);
+free(new);
+return (NULL);
+}
+return (new);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - our struct for dogs
@@ -24,5 +25,6 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /*DOG_H*/
