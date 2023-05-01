5bcb233df770f531e5f22679dd287e15690de05a
Hamza <Hamza@email.com>
2023-05-01 15:16:28 -0700

    struct print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ else if (d->name == NULL)
 printf("Name: (nil)\n");
 else
 printf("Name: %s\n", (*d).name);
-if (d->age == '\0')
+if (d->age < '\0')
 printf("Age: (nil)\n");
 else
 printf("Age: %f\n", (*d).age);
