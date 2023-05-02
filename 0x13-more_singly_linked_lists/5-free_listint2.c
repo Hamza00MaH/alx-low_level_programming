291bcbf6e09854ac87e98921fdeecca59e65cf8d
Hamza <Hamza@email.com>
2023-05-02 14:19:36 -0700

    5 task

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,14 @@
 void free_listint2(listint_t **head)
 {
 listint_t *nod = *head;
-if (*head == NULL)
+if (head == NULL)
 return;
-while (head)
+while (*head)
 {
 nod = (*head)->next;
 free(*head);
 if (nod != NULL)
-*head = nod;
+head = nod;
 else
 head = NULL;
 }
