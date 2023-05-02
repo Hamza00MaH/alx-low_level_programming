cb700849874f0368c2de35e864b4d86cb7df1665
Hamza <Hamza@email.com>
2023-05-02 13:48:51 -0700

    5 task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free linklist
+ * @head:pointer to head of linklist
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *nod;
+if (head == NULL)
+return;  
+while (head != NULL)
+{
+nod = head;
+head = head->next;
+free(nod);
+}
+}
