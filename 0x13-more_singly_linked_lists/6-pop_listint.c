b17f444c9e61de1df483ee7ff19e7166918cd904
Hamza <Hamza@email.com>
2023-05-02 14:36:59 -0700

    6 and 7 task

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the head
+ * @head:pointer to head of linklist
+ * Return: head node data
+ */
+
+int pop_listint(listint_t **head)
+{
+listint_t *nod = *head;
+int data;
+if (*head == NULL)
+return (0);
+*head = (*head)->next;
+data = nod->n;
+free(nod)
+return (0)
+}
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get the linked list much you want
+ * @head: the haed of linkedlist
+ * @index: the number of times of linked list
+ * Return: linked list at the index num
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int count = 0;
+listint_t *nod = head;
+while (nod)
+{
+if(count == index)
+return (nod);
+else
+{
+nod = nod->next;
+count++;
+}
+}
+return (head);
+}
