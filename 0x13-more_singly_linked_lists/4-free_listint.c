fa44f65a07ed92480a043ec92e093f32a927ed3f
Hamza <Hamza@email.com>
2023-05-02 13:42:40 -0700

    freedom the linkedlist

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - free linklist
+ * @head:pointer to head of linklist
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+listint_t *nod;
+while (head != NULL)
+{
+nod = head;
+head = head->next;
+free(nod->n);
+free(nod);
+}
+}
